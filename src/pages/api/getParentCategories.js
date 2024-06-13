86b4a3f4be4a199d8b902e85cdc3bdccf4ce86e2
Duy Khuong <khuongtran227@gmail.com>
2024-06-13 13:55:07 +0700

    c1

--- src/pages/api/getParentCategories.js
+++ src/pages/api/getParentCategories.js
@@ -1,24 +1,29 @@
 // src/pages/api/GetParentCategories.js
-import axios from 'axios';
+import axios from "axios";
 
-export default async function fetchParentCategories(req, res) {
+export default async function handler(req, res) {
   // Set up CORS headers
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept');
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With, Content-Type, Accept"
+  );
 
   // Handle API request
   try {
-    const response = await axios.get('http://ec2-18-143-143-173.ap-southeast-1.compute.amazonaws.com:8080/api/v1/category/anonymous/get-all-parent');
+    const response = await axios.get(
+      "http://ec2-18-143-143-173.ap-southeast-1.compute.amazonaws.com:8080/api/v1/category/anonymous/get-all-parent"
+    );
     const data = response.data;
     console.log("🚀 ~ parent categories data:", data);
     if (response.status === 200) {
       res.status(200).json(data);
     } else {
-      throw new Error('Unexpected status code from API');
+      throw new Error("Unexpected status code from API");
     }
   } catch (error) {
     console.log("🚀 ~ error fetching parent categories:", error);
-    res.status(500).json({ message: 'Internal Server Error' });
+    res.status(500).json({ message: "Internal Server Error" });
   }
 }
